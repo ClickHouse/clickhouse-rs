09d201b6fe61da60060b14ea01a72e927ba11d63
Paul Loyd <pavelko95@gmail.com>
2020-05-08 11:45:16 +0300

    feat: add `Client` and `Insert`

--- src/error.rs
+++ src/error.rs
@@ -6,16 +6,26 @@ pub type Result<T, E = Error> = result::Result<T, E>;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
+    #[error("invalid params: {0}")]
+    InvalidParams(#[source] Box<dyn StdError>),
     #[error("network error: {0}")]
-    Network(Box<dyn StdError>),
+    Network(#[source] Box<dyn StdError>),
     #[error("sequences must have a knowable size ahead of time")]
     SequenceMustHaveLength,
     #[error("a custom error message from serde: {0}")]
     Custom(String),
+    #[error("bad response: {0}")]
+    BadResponse(String),
+}
+
+impl From<hyper::Error> for Error {
+    fn from(error: hyper::Error) -> Self {
+        Self::Network(Box::new(error))
+    }
 }
 
 impl ser::Error for Error {
     fn custom<T: fmt::Display>(msg: T) -> Self {
-        Error::Custom(msg.to_string())
+        Self::Custom(msg.to_string())
     }
 }
---
+++ src/lib.rs
@@ -0,0 +1,134 @@
+use std::marker::PhantomData;
+
+use bytes::BytesMut;
+use hyper::{self, body, client::HttpConnector, Body, Request, Response};
+use serde::Serialize;
+use tokio::task::JoinHandle;
+use url::Url;
+
+use self::error::{Error, Result};
+
+mod error;
+mod rowbinary;
+
+#[derive(Clone)]
+pub struct Client {
+    client: hyper::Client<HttpConnector>,
+
+    url: String,
+    database: Option<String>,
+    user: Option<String>,
+    password: Option<String>,
+}
+
+impl Default for Client {
+    fn default() -> Self {
+        Self {
+            client: hyper::Client::new(),
+            url: String::new(),
+            database: None,
+            user: None,
+            password: None,
+        }
+    }
+}
+
+impl Client {
+    // TODO: use `url` crate?
+    pub fn with_url(mut self, url: impl Into<String>) -> Self {
+        self.url = url.into();
+        self
+    }
+
+    pub fn with_database(mut self, database: impl Into<String>) -> Self {
+        self.database = Some(database.into());
+        self
+    }
+
+    pub fn with_user(mut self, user: impl Into<String>) -> Self {
+        self.user = Some(user.into());
+        self
+    }
+
+    pub fn with_password(mut self, password: impl Into<String>) -> Self {
+        self.password = Some(password.into());
+        self
+    }
+
+    pub fn insert<T>(&self, table: &str) -> Result<Insert<T>> {
+        let mut url = Url::parse(&self.url).expect("TODO");
+        let mut pairs = url.query_pairs_mut();
+        pairs.clear();
+
+        if let Some(database) = &self.database {
+            pairs.append_pair("database", database);
+        }
+
+        // TODO: add fields.
+        // TODO: what about escaping a table name?
+        // https://clickhouse.yandex/docs/en/query_language/syntax/#syntax-identifiers
+        let query = format!("INSERT INTO {}({}) FORMAT RowBinary", table, "TODO");
+        pairs.append_pair("query", &query);
+        drop(pairs);
+
+        let mut builder = Request::post(url.as_str());
+
+        if let Some(user) = &self.user {
+            builder = builder.header("X-ClickHouse-User", user);
+        }
+
+        if let Some(password) = &self.password {
+            builder = builder.header("X-ClickHouse-Key", password);
+        }
+
+        let (sender, body) = Body::channel();
+
+        let request = builder
+            .body(body)
+            .map_err(|err| Error::InvalidParams(Box::new(err)))?;
+
+        let sending = self.client.request(request);
+        let handle = tokio::spawn(sending);
+
+        Ok(Insert {
+            buffer: BytesMut::with_capacity(128 * 1024), // TODO
+            sender,
+            handle,
+            _marker: PhantomData,
+        })
+    }
+}
+
+pub struct Insert<T> {
+    buffer: BytesMut,
+    sender: body::Sender,
+    handle: JoinHandle<hyper::Result<Response<Body>>>,
+    _marker: PhantomData<T>,
+}
+
+impl<T> Insert<T>
+where
+    T: Serialize,
+{
+    pub async fn write(&mut self, row: &T) -> Result<()> {
+        rowbinary::serialize_into(&mut self.buffer, row)?;
+        let chunk = self.buffer.split().freeze();
+        self.sender.send_data(chunk).await?;
+        Ok(())
+    }
+
+    pub async fn end(&mut self) -> Result<()> {
+        // TODO: what about the cancellation case?
+        // TODO: what about panic propagation?
+        let response = (&mut self.handle).await.expect("task panicked")?;
+
+        if !response.status().is_success() {
+            let bytes = body::to_bytes(response.into_body()).await?;
+            let reason = String::from_utf8_lossy(&bytes).into_owned();
+
+            return Err(Error::BadResponse(reason));
+        }
+
+        Ok(())
+    }
+}
