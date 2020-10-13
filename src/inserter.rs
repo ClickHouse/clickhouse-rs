58e1839fca39d7af5ec1f6f043de15d80ba87927
Paul Loyd <pavelko95@gmail.com>
2020-10-13 23:12:21 +0300

    refactor(inserter): change threshold names

--- src/inserter.rs
+++ src/inserter.rs
@@ -5,15 +5,15 @@ use tokio::time::{Duration, Instant};
 
 use crate::{error::Result, insert::Insert, introspection::Reflection, Client};
 
-const DEFAULT_ENTRY_THRESHOLD: u64 = 250_000;
-const DEFAULT_TIME_THRESHOLD: Duration = Duration::from_secs(10);
-const MAX_TIME_BIAS: f64 = 0.10; // % of `time_threshold`
+const DEFAULT_MAX_ENTRIES: u64 = 250_000;
+const DEFAULT_MAX_DURATION: Duration = Duration::from_secs(10);
+const MAX_TIME_BIAS: f64 = 0.10; // % of `max_duration`
 
 pub struct Inserter<T> {
     client: Client,
     table: String,
-    entry_threshold: u64,
-    time_threshold: Duration,
+    max_entries: u64,
+    max_duration: Duration,
     insert: Insert<T>,
     quantities: Quantities,
     next_insert_at: Instant,
@@ -41,35 +41,35 @@ where
         Ok(Self {
             client: client.clone(),
             table: table.into(),
-            entry_threshold: DEFAULT_ENTRY_THRESHOLD,
-            time_threshold: DEFAULT_TIME_THRESHOLD,
+            max_entries: DEFAULT_MAX_ENTRIES,
+            max_duration: DEFAULT_MAX_DURATION,
             insert: client.insert(table)?,
             quantities: Quantities::ZERO,
-            next_insert_at: Instant::now() + DEFAULT_TIME_THRESHOLD,
+            next_insert_at: Instant::now() + DEFAULT_MAX_DURATION,
         })
     }
 
-    pub fn with_entry_threshold(mut self, threshold: u64) -> Self {
-        self.set_entry_threshold(threshold);
+    pub fn with_max_entries(mut self, threshold: u64) -> Self {
+        self.set_max_entries(threshold);
         self
     }
 
-    pub fn with_time_threshold(mut self, threshold: Duration) -> Self {
-        self.set_time_threshold(threshold);
+    pub fn with_max_duration(mut self, threshold: Duration) -> Self {
+        self.set_max_duration(threshold);
         self
     }
 
-    pub fn set_entry_threshold(&mut self, threshold: u64) {
-        self.entry_threshold = threshold;
+    pub fn set_max_entries(&mut self, threshold: u64) {
+        self.max_entries = threshold;
     }
 
-    pub fn set_time_threshold(&mut self, threshold: Duration) {
+    pub fn set_max_duration(&mut self, threshold: Duration) {
         let prev_insert_at = self
             .next_insert_at
-            .checked_sub(self.time_threshold)
+            .checked_sub(self.max_duration)
             .unwrap_or_else(Instant::now);
         self.next_insert_at = prev_insert_at + threshold;
-        self.time_threshold = threshold;
+        self.max_duration = threshold;
     }
 
     #[inline]
@@ -88,7 +88,7 @@ where
         let now = Instant::now();
 
         Ok(if self.is_threshold_reached(now) {
-            self.next_insert_at = shifted_next_time(now, self.next_insert_at, self.time_threshold);
+            self.next_insert_at = shifted_next_time(now, self.next_insert_at, self.max_duration);
             let new_insert = self.client.insert(&self.table)?; // Actually it mustn't fail.
             let insert = mem::replace(&mut self.insert, new_insert);
             insert.end().await?;
@@ -104,16 +104,16 @@ where
     }
 
     fn is_threshold_reached(&self, now: Instant) -> bool {
-        self.quantities.entries >= self.entry_threshold || now >= self.next_insert_at
+        self.quantities.entries >= self.max_entries || now >= self.next_insert_at
     }
 }
 
-fn shifted_next_time(now: Instant, prev: Instant, threshold: Duration) -> Instant {
+fn shifted_next_time(now: Instant, prev: Instant, max_duration: Duration) -> Instant {
     const MAX_TIME_BIAS_255: u32 = (MAX_TIME_BIAS * 255. + 0.5) as u32;
 
     let coef = (now.max(prev) - now.min(prev)).subsec_nanos() & 0xff;
-    let max_bias = threshold * MAX_TIME_BIAS_255 / 255;
+    let max_bias = max_duration * MAX_TIME_BIAS_255 / 255;
     let bias = max_bias * coef / 255;
 
-    prev + threshold + 2 * bias - max_bias
+    prev + max_duration + 2 * bias - max_bias
 }
