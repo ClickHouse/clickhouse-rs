caebf332bfd2f77ae45ec91a83a910bb3dafcbc7
Denis Otkidach <denis.otkidach@gmail.com>
2021-04-11 09:56:39 +0300

    refactor(compression): drop dependency on bytes-05
    Using PR as an example:
    https://github.com/seanmonstar/reqwest/pull/1076/files

--- src/response.rs
+++ src/response.rs
@@ -4,11 +4,17 @@ use std::{
 };
 
 #[cfg(feature = "brotli")]
-use async_compression::stream::BrotliDecoder;
+use async_compression::tokio::bufread::BrotliDecoder;
 #[cfg(feature = "gzip")]
-use async_compression::stream::GzipDecoder;
+use async_compression::tokio::bufread::GzipDecoder;
 #[cfg(feature = "zlib")]
-use async_compression::stream::ZlibDecoder;
+use async_compression::tokio::bufread::ZlibDecoder;
+
+#[cfg(any(feature = "brotli", feature = "gzip", feature = "zlib"))]
+use tokio_util::codec::{BytesCodec, FramedRead};
+#[cfg(any(feature = "brotli", feature = "gzip", feature = "zlib"))]
+use tokio_util::io::StreamReader;
+
 use bytes::Bytes;
 use futures::{
     future,
@@ -78,11 +84,20 @@ where
         #[cfg(feature = "lz4")]
         Compression::Lz4 => Inner::Lz4(Lz4Decoder::new(stream)),
         #[cfg(feature = "gzip")]
-        Compression::Gzip => Inner::Gzip(Box::new(GzipDecoder::new(BodyAdapter(stream)))),
+        Compression::Gzip => Inner::Gzip(FramedRead::new(
+            GzipDecoder::new(StreamReader::new(stream)),
+            BytesCodec::new(),
+        )),
         #[cfg(feature = "zlib")]
-        Compression::Zlib => Inner::Zlib(Box::new(ZlibDecoder::new(BodyAdapter(stream)))),
+        Compression::Zlib => Inner::Zlib(FramedRead::new(
+            ZlibDecoder::new(StreamReader::new(stream)),
+            BytesCodec::new(),
+        )),
         #[cfg(feature = "brotli")]
-        Compression::Brotli => Inner::Brotli(Box::new(BrotliDecoder::new(BodyAdapter(stream)))),
+        Compression::Brotli => Inner::Brotli(FramedRead::new(
+            BrotliDecoder::new(StreamReader::new(stream)),
+            BytesCodec::new(),
+        )),
     })
 }
 
@@ -93,11 +108,11 @@ enum Inner<S> {
     #[cfg(feature = "lz4")]
     Lz4(Lz4Decoder<S>),
     #[cfg(feature = "gzip")]
-    Gzip(Box<GzipDecoder<BodyAdapter<S>>>),
+    Gzip(FramedRead<GzipDecoder<StreamReader<S, Bytes>>, BytesCodec>),
     #[cfg(feature = "zlib")]
-    Zlib(Box<ZlibDecoder<BodyAdapter<S>>>),
+    Zlib(FramedRead<ZlibDecoder<StreamReader<S, Bytes>>, BytesCodec>),
     #[cfg(feature = "brotli")]
-    Brotli(Box<BrotliDecoder<BodyAdapter<S>>>),
+    Brotli(FramedRead<BrotliDecoder<StreamReader<S, Bytes>>, BytesCodec>),
     Empty,
 }
 
@@ -115,11 +130,11 @@ where
             #[cfg(feature = "lz4")]
             Lz4(ref mut inner) => Pin::new(inner).poll_next(cx),
             #[cfg(feature = "gzip")]
-            Gzip(ref mut inner) => map_compression_poll(Pin::new(inner).poll_next(cx)),
+            Gzip(ref mut inner) => Pin::new(inner).poll_next(cx),
             #[cfg(feature = "zlib")]
-            Zlib(ref mut inner) => map_compression_poll(Pin::new(inner).poll_next(cx)),
+            Zlib(ref mut inner) => Pin::new(inner).poll_next(cx),
             #[cfg(feature = "brotli")]
-            Brotli(ref mut inner) => map_compression_poll(Pin::new(inner).poll_next(cx)),
+            Brotli(ref mut inner) => Pin::new(inner).poll_next(cx),
             Empty => Poll::Ready(None),
         };
 
@@ -147,42 +162,6 @@ where
     }
 }
 
-// async-compression depends on bytes-0.5, therefore adapter to convert
-#[cfg(any(feature = "gzip", feature = "zlib", feature = "brotli"))]
-struct BodyAdapter<S>(S);
-
-#[cfg(any(feature = "gzip", feature = "zlib", feature = "brotli"))]
-impl<S, E> Stream for BodyAdapter<S>
-where
-    S: Stream<Item = Result<Bytes, E>> + Unpin,
-    Error: From<E>,
-{
-    type Item = std::io::Result<bytes_05::Bytes>;
-
-    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        Pin::new(&mut self.0).poll_next(cx).map(|opt| {
-            opt.map(|res| {
-                res.map(to_bytes05)
-                    .map_err(|err| Error::from(err).into_io())
-            })
-        })
-    }
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.0.size_hint()
-    }
-}
-
-#[cfg(feature = "bytes-05")]
-fn to_bytes05(bytes: Bytes) -> bytes_05::Bytes {
-    bytes.to_vec().into()
-}
-
-#[cfg(feature = "bytes-05")]
-fn from_bytes05(bytes: bytes_05::Bytes) -> Bytes {
-    bytes.to_vec().into()
-}
-
 // XXX: https://github.com/rust-lang/rust/issues/63514
 fn map_poll_err<T, E, E2>(
     poll: Poll<Option<Result<T, E>>>,
@@ -191,18 +170,6 @@ fn map_poll_err<T, E, E2>(
     match poll {
         Poll::Ready(Some(Ok(val))) => Poll::Ready(Some(Ok(val))),
         Poll::Ready(Some(Err(err))) => Poll::Ready(Some(Err(f(err)))),
-        Poll::Ready(None) => Poll::Ready(None),
-        Poll::Pending => Poll::Pending,
-    }
-}
-
-#[cfg(any(feature = "gzip", feature = "zlib", feature = "brotli"))]
-fn map_compression_poll(
-    poll: Poll<Option<std::io::Result<bytes_05::Bytes>>>,
-) -> Poll<Option<Result<Bytes>>> {
-    match poll {
-        Poll::Ready(Some(Ok(val))) => Poll::Ready(Some(Ok(from_bytes05(val)))),
-        Poll::Ready(Some(Err(err))) => Poll::Ready(Some(Err(Error::decode_io(err)))),
         Poll::Ready(None) => Poll::Ready(None),
         Poll::Pending => Poll::Pending,
     }
