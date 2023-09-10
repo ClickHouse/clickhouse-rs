8b291f0bfc47f7b6c633ffbe062e4e9bc2046a3a
lagrange.eth <lagrange@sorellacapital.com>
2023-09-10 00:39:59 -0700

    test

--- src/fixed_string.rs
+++ src/fixed_string.rs
@@ -1,28 +1,28 @@
+use serde::ser::SerializeStruct;
 use serde::Deserialize;
 use serde::Serialize;
 use serde::Serializer;
-use serde::ser::SerializeStruct;
 
 /// Wrapper type for a FixedString type in Clickhouse
 /// Uses custom serializing handling in SerializeStruct impl for RowBinarySerializer
 /// Forgoes the LEB128 encoding and just encodes the raw byte string
 /// For deserializing the type FixedString(n) with a `query()`, wrap toString(...) around the value
 /// For example:
-/// 
+///
 /// CREATE TABLE test (
 ///     t1 String,
 ///     t2 FixedString(50)
 /// ) ...
-/// 
+///
 /// query("SELECT t1, toString(t2) FROM test;").fetch...
 #[derive(Debug, Deserialize, Clone)]
 pub struct FixedString {
-    pub string: String
+    pub string: String,
 }
 
 impl Serialize for FixedString {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> 
-    where 
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
         S: Serializer,
     {
         let mut state = serializer.serialize_struct("FixedString", 1)?;
@@ -30,7 +30,6 @@ impl Serialize for FixedString {
         state.end()
     }
 }
-
 
 impl FixedString {
     pub fn new(string: String) -> Self {
