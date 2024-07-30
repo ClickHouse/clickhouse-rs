0f6ddc3871fb6e5ae617468af75062c9d3ac2b35
v3xro <175618876+v3xro@users.noreply.github.com>
2024-07-30 17:53:33 +0100

    fix(bind): allow serialization of Some(_) and None values (as the inner value and NULL respectively)

--- src/sql/ser.rs
+++ src/sql/ser.rs
@@ -86,7 +86,6 @@ impl<'a, W: Write> Serializer for SqlSerializer<'a, W> {
     unsupported!(
         serialize_map(Option<usize>) -> Result<Impossible>,
         serialize_bytes(&[u8]),
-        serialize_none,
         serialize_unit,
         serialize_unit_struct(&'static str),
     );
@@ -141,7 +140,13 @@ impl<'a, W: Write> Serializer for SqlSerializer<'a, W> {
 
     #[inline]
     fn serialize_some<T: Serialize + ?Sized>(self, _value: &T) -> Result {
-        Err(SqlSerializerError::Unsupported("serialize_some"))
+        _value.serialize(self)
+    }
+
+    #[inline]
+    fn serialize_none(self) -> std::result::Result<Self::Ok, Self::Error> {
+        self.writer.write_str("NULL")?;
+        Ok(())
     }
 
     #[inline]
