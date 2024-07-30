3ef8b4b0711c7a894c282a8a687856630b72584c
v3xro <175618876+v3xro@users.noreply.github.com>
2024-07-30 16:20:58 +0100

    feat(derive): use rename_all rule for container when naming fields. fixes #71

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,22 +1,27 @@
 use proc_macro2::TokenStream;
 use quote::quote;
+use serde_derive_internals::attr::{Container, RenameRule};
 use serde_derive_internals::{
     attr::{Default as SerdeDefault, Field},
     Ctxt,
 };
 use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields};
 
-fn column_names(data: &DataStruct) -> TokenStream {
+fn column_names(data: &DataStruct, cx: Ctxt, container: &Container) -> TokenStream {
     match &data.fields {
         Fields::Named(fields) => {
-            let cx = Ctxt::new();
+            let rename_rule = container.rename_all_rules().deserialize;
             let column_names_iter = fields
                 .named
                 .iter()
                 .enumerate()
                 .map(|(index, field)| Field::from_ast(&cx, index, field, None, &SerdeDefault::None))
                 .filter(|field| !field.skip_serializing() && !field.skip_deserializing())
-                .map(|field| field.name().serialize_name().to_string());
+                .map(|field| {
+                    rename_rule
+                        .apply_to_field(field.name().serialize_name())
+                        .to_string()
+                });
 
             let tokens = quote! {
                 &[#( #column_names_iter,)*]
@@ -39,10 +44,13 @@ fn column_names(data: &DataStruct) -> TokenStream {
 #[proc_macro_derive(Row)]
 pub fn row(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
+
+    let cx = Ctxt::new();
+    let container = Container::from_ast(&cx, &input);
     let name = input.ident;
 
     let column_names = match &input.data {
-        Data::Struct(data) => column_names(data),
+        Data::Struct(data) => column_names(data, cx, &container),
         Data::Enum(_) | Data::Union(_) => panic!("`Row` can be derived only for structs"),
     };
 
