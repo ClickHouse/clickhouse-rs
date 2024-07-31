97b925e918d9eb6c151a4f2c089ca827e96303b3
v3xro <175618876+v3xro@users.noreply.github.com>
2024-07-31 12:36:34 +0100

    chore: add tests

--- src/sql/ser.rs
+++ src/sql/ser.rs
@@ -345,8 +345,8 @@ mod tests {
     fn it_fails_on_unsupported() {
         let mut out = String::new();
         assert!(write_arg(&mut out, &std::collections::HashMap::<u32, u32>::new()).is_err());
-        assert!(write_arg(&mut out, &None::<u32>).is_err());
-        assert!(write_arg(&mut out, &Some(42)).is_err());
+        assert!(write_arg(&mut out, &None::<u32>).is_ok());
+        assert!(write_arg(&mut out, &Some(42)).is_ok());
         assert!(write_arg(&mut out, &()).is_err());
 
         #[derive(Serialize)]
