dda2d2b100c18ce622052c769d717c1690586391
Serge Klochkov <hypnoash@gmail.com>
2024-08-25 14:19:06 +0300

    Fix unstable feature compile error

--- src/headers.rs
+++ src/headers.rs
@@ -2,14 +2,13 @@ use crate::ProductInfo;
 use hyper::header::USER_AGENT;
 use hyper::http::request::Builder;
 use std::collections::HashMap;
-
-// See https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-crates
-const PKG_VER: &str = option_env!("CARGO_PKG_VERSION").unwrap_or("unknown");
-const RUST_VER: &str = option_env!("CARGO_PKG_RUST_VERSION").unwrap_or("unknown");
-const OS: &str = std::env::consts::OS;
+use std::env::consts::OS;
 
 fn get_user_agent(products_info: &[ProductInfo]) -> String {
-    let default_agent = format!("clickhouse-rs/{PKG_VER} (lv:rust/{RUST_VER}, os:{OS})");
+    // See https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-crates
+    let pkg_ver = option_env!("CARGO_PKG_VERSION").unwrap_or("unknown");
+    let rust_ver = option_env!("CARGO_PKG_RUST_VERSION").unwrap_or("unknown");
+    let default_agent = format!("clickhouse-rs/{pkg_ver} (lv:rust/{rust_ver}, os:{OS})");
     if products_info.is_empty() {
         default_agent
     } else {
