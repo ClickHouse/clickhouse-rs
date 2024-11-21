da27018a2bfe27635a0254cf98903ce6c878e90b
Philip Dubé <serprex@users.noreply.github.com>
2024-11-21 15:29:13 +0000

    with_param (#159)

--- src/sql/bind.rs
+++ src/sql/bind.rs
@@ -8,13 +8,13 @@ use super::{escape, ser};
 #[sealed]
 pub trait Bind {
     #[doc(hidden)]
-    fn write(&self, dst: impl fmt::Write) -> Result<(), String>;
+    fn write(&self, dst: &mut impl fmt::Write) -> Result<(), String>;
 }
 
 #[sealed]
 impl<S: Serialize> Bind for S {
     #[inline]
-    fn write(&self, mut dst: impl fmt::Write) -> Result<(), String> {
+    fn write(&self, mut dst: &mut impl fmt::Write) -> Result<(), String> {
         ser::write_arg(&mut dst, self)
     }
 }
@@ -26,7 +26,7 @@ pub struct Identifier<'a>(pub &'a str);
 #[sealed]
 impl<'a> Bind for Identifier<'a> {
     #[inline]
-    fn write(&self, dst: impl fmt::Write) -> Result<(), String> {
+    fn write(&self, dst: &mut impl fmt::Write) -> Result<(), String> {
         escape::identifier(self.0, dst).map_err(|err| err.to_string())
     }
 }
