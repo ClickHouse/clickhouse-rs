ClickHouse/clickhouse-rs

commit b4cbb3916e11bf9e21d9a3c2bf360422470d4bf3

perf(query): improve SELECTs performance (#169)
* Improve performance of `RowCursor::next()`.
* Add `RowCursor::{decoded_bytes,received_bytes}`  methods.

src/cursor.rs
@@ -5,123 +5,147 @@ use futures::TryStreamExt;
 use serde::Deserialize;
 
 use crate::{
-    buflist::BufList,
+    bytes_ext::BytesExt,
     error::{Error, Result},
-    response::Response,
+    response::{Chunks, Response, ResponseFuture},
     rowbinary,
 };
 
-const INITIAL_BUFFER_SIZE: usize = 1024;
-
 // === RawCursor ===
 
-struct RawCursor {
-    response: Response,
-    pending: BufList<Bytes>,
+struct RawCursor(RawCursorInner);
+
+enum RawCursorInner {
+    Waiting(ResponseFuture),
+    Loading(RawCursorLoading),
+}
+
+struct RawCursorLoading {
+    chunks: Chunks,
+    net_size: u64,
+    data_size: u64,
 }
 
 impl RawCursor {
     fn new(response: Response) -> Self {
-        Self {
-            response,
-            pending: BufList::default(),
-        }
+        Self(RawCursorInner::Waiting(response.into_future()))
     }
 
-    #[inline(always)]
-    async fn next<T>(
-        &mut self,
-        mut f: impl FnMut(&mut BufList<Bytes>) -> ControlFlow<T>,
-    ) -> Result<Option<T>> {
-        let chunks = if let Some(chunks) = self.response.chunks() {
-            chunks
-        } else {
-            self.response.chunks_slow().await?
+    async fn next(&mut self) -> Result<Option<Bytes>> {
+        if matches!(self.0, RawCursorInner::Waiting(_)) {
+            self.resolve().await?;
+        }
+
+        let state = match &mut self.0 {
+            RawCursorInner::Loading(state) => state,
+            RawCursorInner::Waiting(_) => unreachable!(),
         };
 
-        loop {
-            match f(&mut self.pending) {
-                ControlFlow::Yield(value) => {
-                    self.pending.commit();
-                    return Ok(Some(value));
-                }
-                #[cfg(feature = "watch")]
-                ControlFlow::Skip => {
-                    self.pending.commit();
-                    continue;
-                }
-                ControlFlow::Retry => {
-                    self.pending.rollback();
-                    continue;
-                }
-                ControlFlow::Err(Error::NotEnoughData) => {
-                    self.pending.rollback();
-                }
-                ControlFlow::Err(err) => return Err(err),
+        match state.chunks.try_next().await {
+            Ok(Some(chunk)) => {
+                state.net_size += chunk.net_size as u64;
+                state.data_size += chunk.data.len() as u64;
+                Ok(Some(chunk.data))
             }
-
-            match chunks.try_next().await? {
-                Some(chunk) => self.pending.push(chunk),
-                None if self.pending.bufs_cnt() > 0 => return Err(Error::NotEnoughData),
-                None => return Ok(None),
-            }
+            Ok(None) => Ok(None),
+            Err(err) => Err(err),
         }
     }
-}
 
-enum ControlFlow<T> {
-    Yield(T),
-    #[cfg(feature = "watch")]
-    Skip,
-    Retry,
-    Err(Error),
+    async fn resolve(&mut self) -> Result<()> {
+        if let RawCursorInner::Waiting(future) = &mut self.0 {
+            let chunks = future.await;
+            self.0 = RawCursorInner::Loading(RawCursorLoading {
+                chunks: chunks?,
+                net_size: 0,
+                data_size: 0,
+            });
+        }
+        Ok(())
+    }
+
+    fn received_bytes(&self) -> u64 {
+        match &self.0 {
+            RawCursorInner::Waiting(_) => 0,
+            RawCursorInner::Loading(state) => state.net_size,
+        }
+    }
+
+    fn decoded_bytes(&self) -> u64 {
+        match &self.0 {
+            RawCursorInner::Waiting(_) => 0,
+            RawCursorInner::Loading(state) => state.data_size,
+        }
+    }
 }
 
 // XXX: it was a workaround for https://github.com/rust-lang/rust/issues/51132,
 //      but introduced #24 and must be fixed.
-fn workaround_51132<'a, T>(ptr: &mut T) -> &'a mut T {
+fn workaround_51132<'a, T: ?Sized>(ptr: &T) -> &'a T {
     // SAFETY: actually, it leads to unsoundness, see #24
-    unsafe { &mut *(ptr as *mut T) }
+    unsafe { &*(ptr as *const T) }
 }
 
-// === RowBinaryCursor ===
+// === RowCursor ===
 
-pub(crate) struct RowBinaryCursor<T> {
+/// A cursor that emits rows.
+#[must_use]
+pub struct RowCursor<T> {
     raw: RawCursor,
-    buffer: Vec<u8>,
+    bytes: BytesExt,
     _marker: PhantomData<T>,
 }
 
-impl<T> RowBinaryCursor<T> {
+impl<T> RowCursor<T> {
     pub(crate) fn new(response: Response) -> Self {
         Self {
             raw: RawCursor::new(response),
-            buffer: vec![0; INITIAL_BUFFER_SIZE],
+            bytes: BytesExt::default(),
             _marker: PhantomData,
         }
     }
 
-    pub(crate) async fn next<'a, 'b: 'a>(&'a mut self) -> Result<Option<T>>
+    /// Emits the next row.
+    ///
+    /// An result is unspecified if it's called after `Err` is returned.
+    pub async fn next<'a, 'b: 'a>(&'a mut self) -> Result<Option<T>>
     where
         T: Deserialize<'b>,
     {
-        let buffer = &mut self.buffer;
+        loop {
+            let mut slice = workaround_51132(self.bytes.slice());
 
-        self.raw
-            .next(|pending| {
-                match rowbinary::deserialize_from(pending, &mut workaround_51132(buffer)[..]) {
-                    Ok(value) => ControlFlow::Yield(value),
-                    Err(Error::TooSmallBuffer(need)) => {
-                        let new_len = (buffer.len() + need)
-                            .checked_next_power_of_two()
-                            .expect("oom");
-                        buffer.resize(new_len, 0);
-                        ControlFlow::Retry
-                    }
-                    Err(err) => ControlFlow::Err(err),
+            match rowbinary::deserialize_from(&mut slice) {
+                Ok(value) => {
+                    self.bytes.set_remaining(slice.len());
+                    return Ok(Some(value));
                 }
-            })
-            .await
+                Err(Error::NotEnoughData) => {}
+                Err(err) => return Err(err),
+            }
+
+            match self.raw.next().await? {
+                Some(chunk) => self.bytes.extend(chunk),
+                None => return Ok(None),
+            }
+        }
+    }
+
+    /// Returns the total size in bytes received from the CH server since
+    /// the cursor was created.
+    ///
+    /// This method counts only size without HTTP headers for now.
+    /// It can be changed in the future without notice.
+    #[inline]
+    pub fn received_bytes(&self) -> u64 {
+        self.raw.received_bytes()
+    }
+
+    /// Returns the total size in bytes decompressed since the cursor was
+    /// created.
+    #[inline]
+    pub fn decoded_bytes(&self) -> u64 {
+        self.raw.decoded_bytes()
     }
 }
 
@@ -130,6 +154,7 @@ impl<T> RowBinaryCursor<T> {
 #[cfg(feature = "watch")]
 pub(crate) struct JsonCursor<T> {
     raw: RawCursor,
+    bytes: BytesExt,
     line: String,
     _marker: PhantomData<T>,
 }
@@ -146,10 +171,13 @@ enum JsonRow<T> {
 
 #[cfg(feature = "watch")]
 impl<T> JsonCursor<T> {
+    const INITIAL_BUFFER_SIZE: usize = 1024;
+
     pub(crate) fn new(response: Response) -> Self {
         Self {
             raw: RawCursor::new(response),
-            line: String::with_capacity(INITIAL_BUFFER_SIZE),
+            bytes: BytesExt::default(),
+            line: String::with_capacity(Self::INITIAL_BUFFER_SIZE),
             _marker: PhantomData,
         }
     }
@@ -161,32 +189,28 @@ impl<T> JsonCursor<T> {
         use bytes::Buf;
         use std::io::BufRead;
 
-        let line = &mut self.line;
+        loop {
+            self.line.clear();
 
-        self.raw
-            .next(|pending| {
-                line.clear();
-                match pending.reader().read_line(line) {
-                    Ok(_) => {
-                        let line = workaround_51132(line);
-                        if let Some(line) = line.strip_suffix('\n') {
-                            match serde_json::from_str(line) {
-                                Ok(JsonRow::Row(value)) => ControlFlow::Yield(value),
-                                Ok(JsonRow::Progress { .. }) => ControlFlow::Skip,
-                                // TODO: another reason?
-                                Err(err) => ControlFlow::Err(Error::BadResponse(err.to_string())),
-                            }
-                        } else {
-                            ControlFlow::Err(Error::NotEnoughData)
-                        }
-                    }
-                    Err(err) => {
-                        // Actually, it's an unreachable branch, because
-                        // `bytes::buf::Reader` doesn't fail.
-                        ControlFlow::Err(Error::Custom(err.to_string()))
-                    }
+            let read = match self.bytes.slice().reader().read_line(&mut self.line) {
+                Ok(read) => read,
+                Err(err) => return Err(Error::Custom(err.to_string())),
+            };
+
+            if let Some(line) = self.line.strip_suffix('\n') {
+                self.bytes.advance(read);
+
+                match serde_json::from_str(workaround_51132(line)) {
+                    Ok(JsonRow::Row(value)) => return Ok(Some(value)),
+                    Ok(JsonRow::Progress { .. }) => continue,
+                    Err(err) => return Err(Error::BadResponse(err.to_string())),
                 }
-            })
-            .await
+            }
+
+            match self.raw.next().await? {
+                Some(chunk) => self.bytes.extend(chunk),
+                None => return Ok(None),
+            }
+        }
     }
 }

src/rowbinary/de.rs
@@ -8,43 +8,32 @@ use serde::{
 
 use crate::error::{Error, Result};
 
-/// Deserializes a value from `buffer` with a row encoded in `RowBinary`.
-pub(crate) fn deserialize_from<'de, T: Deserialize<'de>>(
-    input: impl Buf,
-    temp_buf: &'de mut [u8],
-) -> Result<T> {
-    let mut deserializer = RowBinaryDeserializer { input, temp_buf };
+/// Deserializes a value from `input` with a row encoded in `RowBinary`.
+///
+/// It accepts _a reference to_ a byte slice because it somehow leads to a more
+/// performant generated code than `(&[u8]) -> Result<(T, usize)>` and even
+/// `(&[u8], &mut Option<T>) -> Result<usize>`.
+pub(crate) fn deserialize_from<'data, T: Deserialize<'data>>(input: &mut &'data [u8]) -> Result<T> {
+    let mut deserializer = RowBinaryDeserializer { input };
     T::deserialize(&mut deserializer)
 }
 
 /// A deserializer for the RowBinary format.
 ///
 /// See https://clickhouse.com/docs/en/interfaces/formats#rowbinary for details.
-struct RowBinaryDeserializer<'de, B> {
-    input: B,
-    temp_buf: &'de mut [u8],
+struct RowBinaryDeserializer<'cursor, 'data> {
+    input: &'cursor mut &'data [u8],
 }
 
-impl<'de, B: Buf> RowBinaryDeserializer<'de, B> {
+impl<'cursor, 'data> RowBinaryDeserializer<'cursor, 'data> {
     fn read_vec(&mut self, size: usize) -> Result<Vec<u8>> {
-        ensure_size(&mut self.input, size)?;
-        let mut vec = vec![0; size];
-        self.input.copy_to_slice(&mut vec[..]);
-        Ok(vec)
+        Ok(self.read_slice(size)?.to_vec())
     }
 
-    fn read_slice(&mut self, size: usize) -> Result<&'de [u8]> {
+    fn read_slice(&mut self, size: usize) -> Result<&'data [u8]> {
         ensure_size(&mut self.input, size)?;
-
-        if self.temp_buf.len() < size {
-            return Err(Error::TooSmallBuffer(size - self.temp_buf.len()));
-        }
-
-        let temp_buf = mem::take(&mut self.temp_buf);
-        let (slice, rest) = temp_buf.split_at_mut(size);
-        self.temp_buf = rest;
-        self.input.copy_to_slice(slice);
-
+        let slice = &self.input[..size];
+        self.input.advance(size);
         Ok(slice)
     }
 
@@ -67,7 +56,7 @@ fn ensure_size(buffer: impl Buf, size: usize) -> Result<()> {
 macro_rules! impl_num {
     ($ty:ty, $deser_method:ident, $visitor_method:ident, $reader_method:ident) => {
         #[inline]
-        fn $deser_method<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value> {
+        fn $deser_method<V: Visitor<'data>>(self, visitor: V) -> Result<V::Value> {
             ensure_size(&mut self.input, mem::size_of::<$ty>())?;
             let value = self.input.$reader_method();
             visitor.$visitor_method(value)
@@ -75,7 +64,7 @@ macro_rules! impl_num {
     };
 }
 
-impl<'de, 'a, B: Buf> Deserializer<'de> for &'a mut RowBinaryDeserializer<'de, B> {
+impl<'cursor, 'data> Deserializer<'data> for &mut RowBinaryDeserializer<'cursor, 'data> {
     type Error = Error;
 
     impl_num!(i8, deserialize_i8, visit_i8, get_i8);
@@ -103,23 +92,23 @@ impl<'de, 'a, B: Buf> Deserializer<'de> for &'a mut RowBinaryDeserializer<'de, B
     impl_num!(f64, deserialize_f64, visit_f64, get_f64_le);
 
     #[inline]
-    fn deserialize_any<V: Visitor<'de>>(self, _: V) -> Result<V::Value> {
+    fn deserialize_any<V: Visitor<'data>>(self, _: V) -> Result<V::Value> {
         Err(Error::DeserializeAnyNotSupported)
     }
 
     #[inline]
-    fn deserialize_unit<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value> {
+    fn deserialize_unit<V: Visitor<'data>>(self, visitor: V) -> Result<V::Value> {
         // TODO: revise this.
         visitor.visit_unit()
     }
 
     #[inline]
-    fn deserialize_char<V: Visitor<'de>>(self, _: V) -> Result<V::Value> {
+    fn deserialize_char<V: Visitor<'data>>(self, _: V) -> Result<V::Value> {
         panic!("character types are unsupported: `char`");
     }
 
     #[inline]
-    fn deserialize_bool<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value> {
+    fn deserialize_bool<V: Visitor<'data>>(self, visitor: V) -> Result<V::Value> {
         ensure_size(&mut self.input, 1)?;
         match self.input.get_u8() {
             0 => visitor.visit_bool(false),
@@ -129,7 +118,7 @@ impl<'de, 'a, B: Buf> Deserializer<'de> for &'a mut RowBinaryDeserializer<'de, B
     }
 
     #[inline]
-    fn deserialize_str<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value> {
+    fn deserialize_str<V: Visitor<'data>>(self, visitor: V) -> Result<V::Value> {
         let size = self.read_size()?;
         let slice = self.read_slice(size)?;
         let str = str::from_utf8(slice).map_err(Error::from)?;
@@ -137,7 +126,7 @@ impl<'de, 'a, B: Buf> Deserializer<'de> for &'a mut RowBinaryDeserializer<'de, B
     }
 
     #[inline]
-    fn deserialize_string<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value> {
+    fn deserialize_string<V: Visitor<'data>>(self, visitor: V) -> Result<V::Value> {
         let size = self.read_size()?;
         let vec = self.read_vec(size)?;
         let string = String::from_utf8(vec).map_err(|err| Error::from(err.utf8_error()))?;
@@ -145,20 +134,20 @@ impl<'de, 'a, B: Buf> Deserializer<'de> for &'a mut RowBinaryDeserializer<'de, B
     }
 
     #[inline]
-    fn deserialize_bytes<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value> {
+    fn deserialize_bytes<V: Visitor<'data>>(self, visitor: V) -> Result<V::Value> {
         let size = self.read_size()?;
         let slice = self.read_slice(size)?;
         visitor.visit_borrowed_bytes(slice)
     }
 
     #[inline]
-    fn deserialize_byte_buf<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value> {
+    fn deserialize_byte_buf<V: Visitor<'data>>(self, visitor: V) -> Result<V::Value> {
         let size = self.read_size()?;
         visitor.visit_byte_buf(self.read_vec(size)?)
     }
 
     #[inline]
-    fn deserialize_enum<V: Visitor<'de>>(
+    fn deserialize_enum<V: Visitor<'data>>(
         self,
         name: &'static str,
         _variants: &'static [&'static str],
@@ -168,18 +157,18 @@ impl<'de, 'a, B: Buf> Deserializer<'de> for &'a mut RowBinaryDeserializer<'de, B
     }
 
     #[inline]
-    fn deserialize_tuple<V: Visitor<'de>>(self, len: usize, visitor: V) -> Result<V::Value> {
-        struct Access<'de, 'a, B> {
-            deserializer: &'a mut RowBinaryDeserializer<'de, B>,
+    fn deserialize_tuple<V: Visitor<'data>>(self, len: usize, visitor: V) -> Result<V::Value> {
+        struct Access<'de, 'cursor, 'data> {
+            deserializer: &'de mut RowBinaryDeserializer<'cursor, 'data>,
             len: usize,
         }
 
-        impl<'de, 'a, B: Buf> SeqAccess<'de> for Access<'de, 'a, B> {
+        impl<'de, 'cursor, 'data> SeqAccess<'data> for Access<'de, 'cursor, 'data> {
             type Error = Error;
 
             fn next_element_seed<T>(&mut self, seed: T) -> Result<Option<T::Value>>
             where
-                T: DeserializeSeed<'de>,
+                T: DeserializeSeed<'data>,
             {
                 if self.len > 0 {
                     self.len -= 1;
@@ -202,7 +191,7 @@ impl<'de, 'a, B: Buf> Deserializer<'de> for &'a mut RowBinaryDeserializer<'de, B
     }
 
     #[inline]
-    fn deserialize_option<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value> {
+    fn deserialize_option<V: Visitor<'data>>(self, visitor: V) -> Result<V::Value> {
         ensure_size(&mut self.input, 1)?;
 
         match self.input.get_u8() {
@@ -213,18 +202,18 @@ impl<'de, 'a, B: Buf> Deserializer<'de> for &'a mut RowBinaryDeserializer<'de, B
     }
 
     #[inline]
-    fn deserialize_seq<V: Visitor<'de>>(self, visitor: V) -> Result<V::Value> {
+    fn deserialize_seq<V: Visitor<'data>>(self, visitor: V) -> Result<V::Value> {
         let len = self.read_size()?;
         self.deserialize_tuple(len, visitor)
     }
 
     #[inline]
-    fn deserialize_map<V: Visitor<'de>>(self, _visitor: V) -> Result<V::Value> {
+    fn deserialize_map<V: Visitor<'data>>(self, _visitor: V) -> Result<V::Value> {
         panic!("maps are unsupported, use `Vec<(A, B)>` instead");
     }
 
     #[inline]
-    fn deserialize_struct<V: Visitor<'de>>(
+    fn deserialize_struct<V: Visitor<'data>>(
         self,
         _name: &str,
         fields: &'static [&'static str],
@@ -234,12 +223,12 @@ impl<'de, 'a, B: Buf> Deserializer<'de> for &'a mut RowBinaryDeserializer<'de, B
     }
 
     #[inline]
-    fn deserialize_identifier<V: Visitor<'de>>(self, _visitor: V) -> Result<V::Value> {
+    fn deserialize_identifier<V: Visitor<'data>>(self, _visitor: V) -> Result<V::Value> {
         panic!("identifiers are unsupported");
     }
 
     #[inline]
-    fn deserialize_newtype_struct<V: Visitor<'de>>(
+    fn deserialize_newtype_struct<V: Visitor<'data>>(
         self,
         _name: &str,
         visitor: V,
@@ -248,7 +237,7 @@ impl<'de, 'a, B: Buf> Deserializer<'de> for &'a mut RowBinaryDeserializer<'de, B
     }
 
     #[inline]
-    fn deserialize_unit_struct<V: Visitor<'de>>(
+    fn deserialize_unit_struct<V: Visitor<'data>>(
         self,
         name: &'static str,
         _visitor: V,
@@ -257,7 +246,7 @@ impl<'de, 'a, B: Buf> Deserializer<'de> for &'a mut RowBinaryDeserializer<'de, B
     }
 
     #[inline]
-    fn deserialize_tuple_struct<V: Visitor<'de>>(
+    fn deserialize_tuple_struct<V: Visitor<'data>>(
         self,
         name: &'static str,
         _len: usize,
@@ -267,7 +256,7 @@ impl<'de, 'a, B: Buf> Deserializer<'de> for &'a mut RowBinaryDeserializer<'de, B
     }
 
     #[inline]
-    fn deserialize_ignored_any<V: Visitor<'de>>(self, _visitor: V) -> Result<V::Value> {
+    fn deserialize_ignored_any<V: Visitor<'data>>(self, _visitor: V) -> Result<V::Value> {
         panic!("ignored types are unsupported");
     }
 

Cargo.toml
@@ -93,7 +93,7 @@ url = "2.1.1"
 futures = "0.3.5"
 futures-channel = "0.3.30"
 static_assertions = "1.1"
-sealed = "0.5"
+sealed = "0.6"
 sha-1 = { version = "0.10", optional = true }
 serde_json = { version = "1.0.68", optional = true }
 lz4_flex = { version = "0.11.3", default-features = false, features = [

benches/common.rs
@@ -1,6 +1,14 @@
 #![allow(dead_code)] // typical for common test/bench modules :(
 
-use std::{convert::Infallible, future::Future, net::SocketAddr, thread};
+use std::{
+    convert::Infallible,
+    future::Future,
+    net::SocketAddr,
+    pin::Pin,
+    sync::atomic::{AtomicU32, Ordering},
+    thread,
+    time::Duration,
+};
 
 use bytes::Bytes;
 use futures::stream::StreamExt;
@@ -11,7 +19,13 @@ use hyper::{
     service, Request, Response,
 };
 use hyper_util::rt::{TokioIo, TokioTimer};
-use tokio::{net::TcpListener, runtime};
+use tokio::{
+    net::TcpListener,
+    runtime,
+    sync::{mpsc, oneshot},
+};
+
+use clickhouse::error::Result;
 
 pub(crate) struct ServerHandle;
 
@@ -38,14 +52,7 @@ where
         }
     };
 
-    thread::spawn(move || {
-        runtime::Builder::new_current_thread()
-            .enable_all()
-            .build()
-            .unwrap()
-            .block_on(serving);
-    });
-
+    run_on_st_runtime("server", serving);
     ServerHandle
 }
 
@@ -56,4 +63,63 @@ pub(crate) async fn skip_incoming(request: Request<Incoming>) {
     while let Some(result) = body.next().await {
         result.unwrap();
     }
+}
+
+pub(crate) struct RunnerHandle {
+    tx: mpsc::UnboundedSender<Run>,
+}
+
+struct Run {
+    future: Pin<Box<dyn Future<Output = Result<Duration>> + Send>>,
+    callback: oneshot::Sender<Result<Duration>>,
+}
+
+impl RunnerHandle {
+    pub(crate) fn run(
+        &self,
+        f: impl Future<Output = Result<Duration>> + Send + 'static,
+    ) -> Duration {
+        let (tx, rx) = oneshot::channel();
+
+        self.tx
+            .send(Run {
+                future: Box::pin(f),
+                callback: tx,
+            })
+            .unwrap();
+
+        rx.blocking_recv().unwrap().unwrap()
+    }
+}
+
+pub(crate) fn start_runner() -> RunnerHandle {
+    let (tx, mut rx) = mpsc::unbounded_channel::<Run>();
+
+    run_on_st_runtime("testee", async move {
+        while let Some(run) = rx.recv().await {
+            let result = run.future.await;
+            let _ = run.callback.send(result);
+        }
+    });
+
+    RunnerHandle { tx }
+}
+
+fn run_on_st_runtime(name: &str, f: impl Future + Send + 'static) {
+    let name = name.to_string();
+    thread::Builder::new()
+        .name(name.clone())
+        .spawn(move || {
+            let no = AtomicU32::new(0);
+            runtime::Builder::new_current_thread()
+                .enable_all()
+                .thread_name_fn(move || {
+                    let no = no.fetch_add(1, Ordering::Relaxed);
+                    format!("{name}-{no}")
+                })
+                .build()
+                .unwrap()
+                .block_on(f);
+        })
+        .unwrap();
 }

benches/insert.rs
@@ -1,11 +1,14 @@
-use std::{future::Future, mem, time::Duration};
+use std::{
+    future::Future,
+    mem,
+    time::{Duration, Instant},
+};
 
 use bytes::Bytes;
 use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
 use http_body_util::Empty;
 use hyper::{body::Incoming, Request, Response};
 use serde::Serialize;
-use tokio::{runtime::Runtime, time::Instant};
 
 use clickhouse::{error::Result, Client, Compression, Row};
 
@@ -76,30 +79,29 @@ async fn run_inserter<const WITH_PERIOD: bool>(client: Client, iters: u64) -> Re
 
 fn run<F>(c: &mut Criterion, name: &str, port: u16, f: impl Fn(Client, u64) -> F)
 where
-    F: Future<Output = Result<Duration>>,
+    F: Future<Output = Result<Duration>> + Send + 'static,
 {
     let addr = format!("127.0.0.1:{port}").parse().unwrap();
     let _server = common::start_server(addr, serve);
+    let runner = common::start_runner();
 
     let mut group = c.benchmark_group(name);
     group.throughput(Throughput::Bytes(mem::size_of::<SomeRow>() as u64));
     group.bench_function("no compression", |b| {
         b.iter_custom(|iters| {
-            let rt = Runtime::new().unwrap();
             let client = Client::default()
                 .with_url(format!("http://{addr}"))
                 .with_compression(Compression::None);
-            rt.block_on((f)(client, iters)).unwrap()
+            runner.run((f)(client, iters))
         })
     });
     #[cfg(feature = "lz4")]
     group.bench_function("lz4", |b| {
         b.iter_custom(|iters| {
-            let rt = Runtime::new().unwrap();
             let client = Client::default()
                 .with_url(format!("http://{addr}"))
                 .with_compression(Compression::Lz4);
-            rt.block_on((f)(client, iters)).unwrap()
+            runner.run((f)(client, iters))
         })
     });
     group.finish();

benches/select.rs
@@ -1,4 +1,8 @@
-use std::{convert::Infallible, mem};
+use std::{
+    convert::Infallible,
+    mem,
+    time::{Duration, Instant},
+};
 
 use bytes::Bytes;
 use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
@@ -9,7 +13,6 @@ use hyper::{
     Request, Response,
 };
 use serde::Deserialize;
-use tokio::{runtime::Runtime, time::Instant};
 
 use clickhouse::{error::Result, Client, Compression, Row};
 
@@ -47,6 +50,7 @@ fn select(c: &mut Criterion) {
     let addr = "127.0.0.1:6543".parse().unwrap();
     let chunk = prepare_chunk();
     let _server = common::start_server(addr, move |req| serve(req, chunk.clone()));
+    let runner = common::start_runner();
 
     #[allow(dead_code)]
     #[derive(Debug, Row, Deserialize)]
@@ -57,7 +61,8 @@ fn select(c: &mut Criterion) {
         d: u32,
     }
 
-    async fn run(client: Client, iters: u64) -> Result<()> {
+    async fn run(client: Client, iters: u64) -> Result<Duration> {
+        let start = Instant::now();
         let mut cursor = client
             .query("SELECT ?fields FROM some")
             .fetch::<SomeRow>()?;
@@ -66,32 +71,26 @@ fn select(c: &mut Criterion) {
             black_box(cursor.next().await?);
         }
 
-        Ok(())
+        Ok(start.elapsed())
     }
 
     let mut group = c.benchmark_group("select");
     group.throughput(Throughput::Bytes(mem::size_of::<SomeRow>() as u64));
     group.bench_function("no compression", |b| {
         b.iter_custom(|iters| {
-            let rt = Runtime::new().unwrap();
             let client = Client::default()
                 .with_url(format!("http://{addr}"))
                 .with_compression(Compression::None);
-            let start = Instant::now();
-            rt.block_on(run(client, iters)).unwrap();
-            start.elapsed()
+            runner.run(run(client, iters))
         })
     });
     #[cfg(feature = "lz4")]
     group.bench_function("lz4", |b| {
         b.iter_custom(|iters| {
-            let rt = Runtime::new().unwrap();
             let client = Client::default()
                 .with_url(format!("http://{addr}"))
                 .with_compression(Compression::Lz4);
-            let start = Instant::now();
-            rt.block_on(run(client, iters)).unwrap();
-            start.elapsed()
+            runner.run(run(client, iters))
         })
     });
     group.finish();

commit 955e81970a0340e28fc09759bc653c126d4997eb

fix(cursor): return NotEnoughData
It fixes the regression introduced in #169.
Previously, the cursor checked if unhandled bytes were left.
Return this behaviour to detect schema mismatch.

Closes #185

src/cursor.rs
@@ -126,6 +126,11 @@ impl<T> RowCursor<T> {
 
             match self.raw.next().await? {
                 Some(chunk) => self.bytes.extend(chunk),
+                None if self.bytes.remaining() > 0 => {
+                    // If some data is left, we have an incomplete row in the buffer.
+                    // This is usually a schema mismatch on the client side.
+                    return Err(Error::NotEnoughData);
+                }
                 None => return Ok(None),
             }
         }

src/rowbinary/de.rs
@@ -2,9 +2,8 @@ use std::{convert::TryFrom, mem, str};
 
 use crate::error::{Error, Result};
 use bytes::Buf;
-use serde::de::{EnumAccess, VariantAccess};
 use serde::{
-    de::{DeserializeSeed, Deserializer, SeqAccess, Visitor},
+    de::{DeserializeSeed, Deserializer, EnumAccess, SeqAccess, VariantAccess, Visitor},
     Deserialize,
 };
 

tests/it/cursor_error.rs
@@ -1,4 +1,6 @@
-use clickhouse::{Client, Compression};
+use serde::Deserialize;
+
+use clickhouse::{error::Error, Client, Compression, Row};
 
 #[tokio::test]
 async fn deferred() {
@@ -95,4 +97,41 @@ async fn deferred_lz4() {
 
     assert_ne!(i, 0); // we're interested only in errors during processing
     assert!(err.to_string().contains("TIMEOUT_EXCEEDED"));
+}
+
+// See #185.
+#[tokio::test]
+async fn invalid_schema() {
+    #[derive(Debug, Row, Deserialize)]
+    #[allow(dead_code)]
+    struct MyRow {
+        no: u32,
+        dec: Option<String>, // valid schema: u64-based types
+    }
+
+    let client = prepare_database!();
+
+    client
+        .query(
+            "CREATE TABLE test(no UInt32, dec Nullable(Decimal64(4)))
+             ENGINE = MergeTree
+             ORDER BY no",
+        )
+        .execute()
+        .await
+        .unwrap();
+
+    client
+        .query("INSERT INTO test VALUES (1, 1.1), (2, 2.2), (3, 3.3)")
+        .execute()
+        .await
+        .unwrap();
+
+    let err = client
+        .query("SELECT ?fields FROM test")
+        .fetch_all::<MyRow>()
+        .await
+        .unwrap_err();
+
+    assert!(matches!(err, Error::NotEnoughData));
 }