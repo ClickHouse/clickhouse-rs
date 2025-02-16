6cab9aa97b3a77d4c6667de77e52432100a3b3b4
Paul Loyd <pavelko95@gmail.com>
2025-02-16 13:44:54 +0400

    --wip-- [skip ci]

--- src/cursors/raw.rs
+++ src/cursors/raw.rs
@@ -3,8 +3,11 @@ use crate::{
     response::{Chunks, Response, ResponseFuture},
 };
 use bytes::Bytes;
-use futures::{FutureExt, TryStreamExt};
-use std::task::{Context, Poll};
+use futures::Stream;
+use std::{
+    pin::pin,
+    task::{ready, Context, Poll},
+};
 
 /// A cursor over raw bytes of a query response.
 /// All other cursors are built on top of this one.
@@ -27,41 +30,7 @@ impl RawCursor {
     }
 
     pub(crate) async fn next(&mut self) -> Result<Option<Bytes>> {
-        if matches!(self.0, RawCursorState::Waiting(_)) {
-            self.resolve().await?;
-        }
-
-        let state = match &mut self.0 {
-            RawCursorState::Loading(state) => state,
-            RawCursorState::Waiting(_) => unreachable!(),
-        };
-
-        match state.chunks.try_next().await {
-            Ok(Some(chunk)) => {
-                state.net_size += chunk.net_size as u64;
-                state.data_size += chunk.data.len() as u64;
-                Ok(Some(chunk.data))
-            }
-            Ok(None) => Ok(None),
-            Err(err) => Err(err),
-        }
-    }
-
-    pub(crate) fn poll_next(&mut self, cx: &mut Context<'_>) -> Poll<Result<Option<Bytes>>> {
-        // TODO: implement `next()` based on `poll_next()` and avoid `boxed()`.
-        self.next().boxed().poll_unpin(cx)
-    }
-
-    async fn resolve(&mut self) -> Result<()> {
-        if let RawCursorState::Waiting(future) = &mut self.0 {
-            let chunks = future.await;
-            self.0 = RawCursorState::Loading(RawCursorLoading {
-                chunks: chunks?,
-                net_size: 0,
-                data_size: 0,
-            });
-        }
-        Ok(())
+        std::future::poll_fn(|cx| self.poll_next(cx)).await
     }
 
     pub(crate) fn received_bytes(&self) -> u64 {
@@ -76,5 +45,40 @@ impl RawCursor {
             RawCursorState::Waiting(_) => 0,
             RawCursorState::Loading(state) => state.data_size,
         }
+    }
+
+    pub(crate) fn poll_next(&mut self, cx: &mut Context<'_>) -> Poll<Result<Option<Bytes>>> {
+        if matches!(self.0, RawCursorState::Waiting(_)) {
+            ready!(self.poll_resolve(cx)?);
+        }
+
+        let state = match &mut self.0 {
+            RawCursorState::Loading(state) => state,
+            RawCursorState::Waiting(_) => unreachable!(),
+        };
+
+        let chunks = pin!(&mut state.chunks);
+
+        Poll::Ready(match ready!(chunks.poll_next(cx)?) {
+            Some(chunk) => {
+                state.net_size += chunk.net_size as u64;
+                state.data_size += chunk.data.len() as u64;
+                Ok(Some(chunk.data))
+            }
+            None => Ok(None),
+        })
+    }
+
+    #[cold]
+    fn poll_resolve(&mut self, cx: &mut Context<'_>) -> Poll<Result<()>> {
+        if let RawCursorState::Waiting(future) = &mut self.0 {
+            let chunks = ready!(future.as_mut().poll(cx)?);
+            self.0 = RawCursorState::Loading(RawCursorLoading {
+                chunks,
+                net_size: 0,
+                data_size: 0,
+            });
+        }
+        Poll::Ready(Ok(()))
     }
 }
