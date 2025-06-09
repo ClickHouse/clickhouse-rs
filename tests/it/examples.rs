38d771d98e67c6b1424bbdabf3980f6dfe6eaa7b
slvrtrn <hypnoash@gmail.com>
2025-06-09 23:52:49 +0200

    Clippy fixes

--- tests/it/examples.rs
+++ tests/it/examples.rs
@@ -4,19 +4,19 @@ fn test_all_examples_exit_zero() {
     for entry in entries {
         let entry = entry.unwrap();
         let path = entry.path();
-        if path.is_file() && path.extension().map_or(false, |ext| ext == "rs") {
+        if path.is_file() && path.extension().is_some_and(|ext| ext == "rs") {
             let file_name = path.file_stem().unwrap().to_str().unwrap();
             if !file_name.ends_with("_test.rs") {
                 println!("-- Running example: {}", file_name);
                 let output = std::process::Command::new("cargo")
-                    .args(&["run", "--example", file_name, "--all-features"])
+                    .args(["run", "--example", file_name, "--all-features"])
                     .envs([
                         ("CLICKHOUSE_URL", "http://localhost:8123"),
                         ("CLICKHOUSE_USER", "default"),
                         ("CLICKHOUSE_PASSWORD", ""),
                     ])
                     .output()
-                    .expect(&format!("Failed to execute example {}", file_name));
+                    .unwrap_or_else(|_| panic!("Failed to execute example {}", file_name));
                 assert!(
                     output.status.success(),
                     "Example '{}' failed with stderr: {}",
