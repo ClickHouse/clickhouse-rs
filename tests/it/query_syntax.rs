082f7b12190a4c893969a1d1837516a40b2c8973
restrry <restrry@gmail.com>
2025-08-03 11:29:39 +0200

    make fmt happy

--- tests/it/query_syntax.rs
+++ tests/it/query_syntax.rs
@@ -8,28 +8,44 @@ async fn query_with_semicolon() {
 #[tokio::test]
 async fn query_with_tail_comment() {
     let client = prepare_database!();
-    let value = client.query("SELECT 1 \n --comment").fetch_one::<u8>().await.unwrap();
+    let value = client
+        .query("SELECT 1 \n --comment")
+        .fetch_one::<u8>()
+        .await
+        .unwrap();
     assert_eq!(value, 1);
 }
 
 #[tokio::test]
 async fn query_with_head_comment() {
     let client = prepare_database!();
-    let value = client.query("--comment\nSELECT 1").fetch_one::<u8>().await.unwrap();
+    let value = client
+        .query("--comment\nSELECT 1")
+        .fetch_one::<u8>()
+        .await
+        .unwrap();
     assert_eq!(value, 1);
 }
 
 #[tokio::test]
 async fn query_with_head_and_tail_comment() {
     let client = prepare_database!();
-    let value = client.query("--comment\nSELECT 1\n--comment").fetch_one::<u8>().await.unwrap();
+    let value = client
+        .query("--comment\nSELECT 1\n--comment")
+        .fetch_one::<u8>()
+        .await
+        .unwrap();
     assert_eq!(value, 1);
 }
 
 #[tokio::test]
 async fn query_with_mid_comment() {
     let client = prepare_database!();
-    let value = client.query("SELECT \n--comment\n 1").fetch_one::<u8>().await.unwrap();
+    let value = client
+        .query("SELECT \n--comment\n 1")
+        .fetch_one::<u8>()
+        .await
+        .unwrap();
     assert_eq!(value, 1);
 }
 
@@ -64,7 +80,11 @@ async fn query_with_tail_carriage_return() {
 #[tokio::test]
 async fn query_with_tail_carriage_return_newline() {
     let client = prepare_database!();
-    let value = client.query("SELECT 1\r\n").fetch_one::<u8>().await.unwrap();
+    let value = client
+        .query("SELECT 1\r\n")
+        .fetch_one::<u8>()
+        .await
+        .unwrap();
     assert_eq!(value, 1);
 }
 
