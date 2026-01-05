a9ed86f0581b45aefcebe64aa68a1d77670be441
Austin Bonander <austin.bonander@gmail.com>
2026-01-05 07:55:18 -0800

    refactor(tests/it): move `#[cfg(feature)]` where it's normally expected
    It was confusing RustRover where it was.

--- tests/it/inserter.rs
+++ tests/it/inserter.rs
@@ -1,5 +1,3 @@
-#![cfg(feature = "inserter")]
-
 use std::string::ToString;
 
 use serde::Serialize;
--- tests/it/main.rs
+++ tests/it/main.rs
@@ -255,6 +255,7 @@ mod fetch_bytes;
 mod https_errors;
 mod insert;
 mod insert_formatted;
+#[cfg(feature = "inserter")]
 mod inserter;
 mod int128;
 mod int256;
