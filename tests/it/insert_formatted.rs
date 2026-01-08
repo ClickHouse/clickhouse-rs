a0a6db06f6ddbd131f565c0664017aaf3e82be46
Austin Bonander <austin.bonander@gmail.com>
2026-01-07 17:36:18 -0800

    chore: add test for `InsertFormatted::with_timeouts()`

--- tests/it/insert_formatted.rs
+++ tests/it/insert_formatted.rs
@@ -1,10 +1,13 @@
 use crate::{SimpleRow, create_simple_table, fetch_rows};
 use bytes::{Buf, Bytes};
+use clickhouse::error::Error;
 use clickhouse::{Client, Compression};
 use clickhouse_macros::Row;
 use serde::Deserialize;
 use std::cmp;
+use std::time::Duration;
 use tokio::io::AsyncWriteExt;
+use tokio::net::TcpListener;
 
 /// First 1000 records of the [NYC taxi dataset] in `TabSeparated` format.
 ///
@@ -95,6 +98,84 @@ async fn insert_compressed() {
 }
 
 #[tokio::test]
+async fn insert_send_timeout() {
+    // Simulate a timeout by attempting to send a request to a server
+    // that's listening but not answering.
+    let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+    let local_addr = listener.local_addr().unwrap();
+
+    let client = Client::default().with_url(format!("http://{local_addr}"));
+
+    let bytes = Bytes::copy_from_slice(TAXI_DATA_TSV);
+
+    let send_timeout = Duration::from_millis(100);
+
+    let mut insert = client
+        .insert_formatted_with("INSERT INTO nyc_taxi_trips_small FORMAT TabSeparated")
+        .with_timeouts(Some(send_timeout), None);
+
+    // First send actually initiates the request.
+    if let Err(e) = insert.send(bytes.clone()).await {
+        assert!(
+            matches!(e, Error::TimedOut),
+            "expected `Err(TimedOut)`, got {e:?}"
+        );
+    }
+
+    // We have to accept the socket
+    let _socket = listener.accept().await.unwrap();
+
+    for _ in 0..1024 {
+        // It might take a few sends to fill up the TCP send window.
+        if let Err(e) = insert.send(bytes.clone()).await {
+            assert!(
+                matches!(e, Error::TimedOut),
+                "expected `Err(TimedOut)`, got {e:?}"
+            );
+            return;
+        }
+    }
+
+    unreachable!("BUG: `send_timeout` should have triggered by now!");
+}
+
+#[tokio::test]
+async fn insert_end_timeout() {
+    // Simulate a timeout by attempting to send a request to a server
+    // that's listening but not answering.
+    let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+    let local_addr = listener.local_addr().unwrap();
+
+    let client = Client::default().with_url(format!("http://{local_addr}"));
+
+    let bytes = Bytes::copy_from_slice(TAXI_DATA_TSV);
+
+    let end_timeout = Duration::from_millis(100);
+
+    let mut insert = client
+        .insert_formatted_with("INSERT INTO nyc_taxi_trips_small FORMAT TabSeparated")
+        .with_timeouts(None, Some(end_timeout));
+
+    // First send actually initiates the request.
+    if let Err(e) = insert.send(bytes.clone()).await {
+        assert!(
+            matches!(e, Error::TimedOut),
+            "expected `Err(TimedOut)`, got {e:?}"
+        );
+    }
+
+    // We have to accept the socket
+    let _socket = listener.accept().await.unwrap();
+
+    if let Err(e) = insert.end().await {
+        assert!(
+            matches!(e, Error::TimedOut),
+            "expected `Err(TimedOut)`, got {e:?}"
+        );
+    }
+}
+
+#[tokio::test]
 async fn insert_buffered() {
     let client = prepare_database!();
 
@@ -123,6 +204,79 @@ async fn insert_buffered() {
             assert_ne!(written, 0);
             data = &data[written..];
         }
+    }
+
+    insert.end().await.unwrap();
+
+    verify_insert(&client).await;
+}
+
+#[tokio::test]
+async fn insert_buffered_zero_capacity() {
+    let client = prepare_database!();
+
+    create_table(&client).await;
+
+    let mut data = TAXI_DATA_TSV;
+
+    let mut insert = client
+        .insert_formatted_with("INSERT INTO nyc_taxi_trips_small FORMAT TabSeparated")
+        .buffered_with_capacity(0);
+
+    // Cycle different read sizes in an attempt to break the buffer
+    let read_sizes = [1, 10, 100, 1000, 1024];
+
+    while !data.is_empty() {
+        for size in read_sizes {
+            if data.is_empty() {
+                break;
+            }
+
+            let written = insert
+                .write(&data[..cmp::min(size, data.len())])
+                .await
+                .unwrap();
+            assert_ne!(written, 0);
+            data = &data[written..];
+        }
+    }
+
+    insert.end().await.unwrap();
+
+    verify_insert(&client).await;
+}
+
+#[tokio::test]
+async fn insert_write_buffered() {
+    let client = prepare_database!();
+
+    create_table(&client).await;
+
+    let mut data = TAXI_DATA_TSV;
+
+    let capacity = 8192;
+
+    let mut insert = client
+        .insert_formatted_with("INSERT INTO nyc_taxi_trips_small FORMAT TabSeparated")
+        .buffered_with_capacity(capacity);
+
+    // Cycle different read sizes in an attempt to break the buffer
+    let read_sizes = [1, 10, 100, 1000, 1024, capacity];
+
+    while !data.is_empty() {
+        for size in read_sizes {
+            if data.is_empty() {
+                break;
+            }
+
+            let write_len = cmp::min(size, data.len());
+
+            insert.write_buffered(&data[..write_len]);
+
+            data = &data[write_len..];
+        }
+
+        insert.flush().await.unwrap();
     }
 
     insert.end().await.unwrap();
