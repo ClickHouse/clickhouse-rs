fd08ee37fd549923987987cd758c7cd7bcf6e7f6
JaminMartin <jamin.martin1@gmail.com>
2026-03-09 13:14:22 +1300

    feat: added support for array of UUID's

--- tests/it/uuid.rs
+++ tests/it/uuid.rs
@@ -80,3 +80,68 @@ async fn human_readable_smoke() {
 
     assert_eq!(new_row2, row2);
 }
+#[tokio::test]
+async fn vec_smoke() {
+    let client = prepare_database!();
+
+    #[derive(Debug, PartialEq, Eq, Serialize, Deserialize, Row)]
+    struct MyRow {
+        #[serde(with = "clickhouse::serde::uuid_vec")]
+        uuids: Vec<Uuid>,
+    }
+
+    client
+        .query(
+            "
+            CREATE TABLE test(
+                uuids Array(UUID)
+            ) ENGINE = MergeTree ORDER BY uuids
+        ",
+        )
+        .execute()
+        .await
+        .unwrap();
+
+    let uuids = vec![Uuid::new_v4(), Uuid::new_v4()];
+    println!("uuids: {uuids:?}");
+
+    let original_row = MyRow { uuids };
+
+    let mut insert = client.insert::<MyRow>("test").await.unwrap();
+    insert.write(&original_row).await.unwrap();
+    insert.end().await.unwrap();
+
+    let row = client
+        .query("SELECT ?fields FROM test")
+        .fetch_one::<MyRow>()
+        .await
+        .unwrap();
+
+    assert_eq!(row, original_row);
+}
+#[tokio::test]
+async fn vec_human_readable_smoke() {
+    #[derive(Debug, PartialEq, Eq, Serialize, Deserialize, Row)]
+    struct OursRow {
+        #[serde(with = "clickhouse::serde::uuid_vec")]
+        uuids: Vec<Uuid>,
+    }
+
+    #[derive(Debug, PartialEq, Eq, Serialize, Deserialize, Row)]
+    struct TheirsRow {
+        uuids: Vec<Uuid>,
+    }
+
+    let uuids = vec![Uuid::new_v4(), Uuid::new_v4()];
+    let row = OursRow {
+        uuids: uuids.clone(),
+    };
+    let row2 = TheirsRow { uuids };
+
+    let s1 = serde_json::to_string(&row).unwrap();
+    let s2 = serde_json::to_string(&row2).unwrap();
+    assert_eq!(s1, s2);
+
+    let new_row2: TheirsRow = serde_json::from_str(&s2).unwrap();
+    assert_eq!(new_row2, row2);
+}
