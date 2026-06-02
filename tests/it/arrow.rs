176c14e776cc97f1b2fbe9e90b188b9c7279d831
Austin Bonander <austin.bonander@clickhouse.com>
2026-06-02 14:01:24 -0700

    feat: add `ArrowClientExt::insert_arrow_with()` (#436)

--- tests/it/arrow.rs
+++ tests/it/arrow.rs
@@ -146,6 +146,97 @@ async fn insert() {
 }
 
 #[tokio::test]
+async fn insert_custom() {
+    let client = prepare_database!();
+
+    client
+        .query(
+            "CREATE TABLE arrow_custom_insert_test(bar Int32, baz String) ENGINE = MergeTree ORDER BY bar",
+        )
+        .execute()
+        .await
+        .unwrap();
+
+    let batch_size = 100;
+    let num_batches = 100;
+    let mut next_id = 1..;
+
+    let mut batches = Vec::new();
+    let schema = Arc::new(Schema::new(vec![
+        Field::new("bar", DataType::Int32, false),
+        Field::new("baz", DataType::Utf8, false),
+    ]));
+
+    for batch in 1..=num_batches {
+        let bars: PrimitiveArray<Int32Type> = (0..batch_size)
+            .zip(&mut next_id)
+            .map(|(_, id)| id)
+            .collect();
+
+        let bazzes: StringArray = bars
+            .iter()
+            .filter_map(|bar| {
+                let bar = bar?;
+                Some(format!("batch_{batch}_bar_{bar}"))
+            })
+            .collect::<Vec<String>>()
+            .into();
+
+        batches.push(
+            RecordBatch::try_new(schema.clone(), vec![Arc::new(bars), Arc::new(bazzes)]).unwrap(),
+        );
+    }
+
+    let mut insert = client
+        .insert_arrow_with("INSERT INTO arrow_custom_insert_test(bar, baz) FORMAT ArrowStream");
+
+    for batch in batches {
+        insert.write(&batch).await.unwrap();
+    }
+
+    insert.end().await.unwrap();
+
+    let result = client
+        .query(
+            "SELECT \
+         count(*) AS row_count, \
+         first_value(bar) AS min_bar, \
+         first_value(baz) AS min_baz,
+         last_value(bar) AS max_bar, \
+         last_value(baz) AS max_baz \
+         FROM (SELECT * FROM arrow_custom_insert_test ORDER BY bar)",
+        )
+        .fetch_arrow()
+        .unwrap()
+        .collect_merged()
+        .await
+        .unwrap();
+
+    let expected_count = batch_size * num_batches;
+
+    let expected = RecordBatch::try_new(
+        Schema::new(vec![
+            Field::new("row_count", DataType::UInt64, false),
+            Field::new("min_bar", DataType::Int32, false),
+            Field::new("min_baz", DataType::Utf8, false),
+            Field::new("max_bar", DataType::Int32, false),
+            Field::new("max_baz", DataType::Utf8, false),
+        ])
+        .into(),
+        vec![
+            create_array!(UInt64, [expected_count]),
+            create_array!(Int32, [1]),
+            create_array!(Utf8, ["batch_1_bar_1"]),
+            create_array!(Int32, [expected_count as i32]),
+            create_array!(Utf8, ["batch_100_bar_10000"]),
+        ],
+    )
+    .unwrap();
+
+    assert_eq!(result, expected);
+}
+
+#[tokio::test]
 async fn query_empty_response() {
     let client = get_client();
 
