6d52aa4133783c8c546327bdf83e8970a4e956c6
Jamin Martin <33270052+JaminMartin@users.noreply.github.com>
2026-06-24 07:06:37 +1200

    Added support for Vec<UUID> // Array(UUID)  (#424)
    Co-authored-by: JaminMartin <jamin@dawnaerospace.com>

--- tests/it/uuid.rs
+++ tests/it/uuid.rs
@@ -80,3 +80,95 @@ async fn human_readable_smoke() {
 
     assert_eq!(new_row2, row2);
 }
+#[tokio::test]
+async fn vec_smoke() {
+    let client = prepare_database!();
+
+    #[derive(Debug, PartialEq, Eq, Serialize, Deserialize, Row)]
+    struct MyRow {
+        #[serde(with = "clickhouse::serde::uuid_vec")]
+        uuids: Vec<Uuid>,
+    }
+
+    client
+        .query(
+            "
+            CREATE TABLE test(
+                uuids Array(UUID)
+            ) ENGINE = MergeTree ORDER BY uuids
+        ",
+        )
+        .execute()
+        .await
+        .unwrap();
+
+    let uuids = vec![Uuid::new_v4(), Uuid::new_v4()];
+    println!("uuids: {uuids:?}");
+
+    let original_row = MyRow { uuids };
+
+    let mut insert = client.insert::<MyRow>("test").await.unwrap();
+    insert.write(&original_row).await.unwrap();
+    insert.end().await.unwrap();
+
+    let row = client
+        .query("SELECT ?fields FROM test")
+        .fetch_one::<MyRow>()
+        .await
+        .unwrap();
+
+    assert_eq!(row, original_row);
+}
+#[tokio::test]
+async fn vec_human_readable_smoke() {
+    #[derive(Debug, PartialEq, Eq, Serialize, Deserialize, Row)]
+    struct OursRow {
+        #[serde(with = "clickhouse::serde::uuid_vec")]
+        uuids: Vec<Uuid>,
+        #[serde(with = "clickhouse::serde::uuid_vec::option")]
+        uuids_opt: Option<Vec<Uuid>>,
+    }
+
+    #[derive(Debug, PartialEq, Eq, Serialize, Deserialize, Row)]
+    struct TheirsRow {
+        uuids: Vec<Uuid>,
+        uuids_opt: Option<Vec<Uuid>>,
+    }
+
+    let uuids = vec![Uuid::new_v4(), Uuid::new_v4()];
+    let uuids_opt = Some(vec![Uuid::new_v4(), Uuid::new_v4()]);
+    let row = OursRow {
+        uuids: uuids.clone(),
+        uuids_opt: uuids_opt.clone(),
+    };
+    let row2 = TheirsRow { uuids, uuids_opt };
+
+    let s1 = serde_json::to_string(&row).unwrap();
+    let s2 = serde_json::to_string(&row2).unwrap();
+    assert_eq!(s1, s2);
+
+    let new_row2: TheirsRow = serde_json::from_str(&s2).unwrap();
+    assert_eq!(new_row2, row2);
+
+    let new_row: OursRow = serde_json::from_str(&s2).unwrap();
+    assert_eq!(new_row, row);
+
+    let row = OursRow {
+        uuids: Vec::new(),
+        uuids_opt: None,
+    };
+    let row2 = TheirsRow {
+        uuids: Vec::new(),
+        uuids_opt: None,
+    };
+
+    let s1 = serde_json::to_string(&row).unwrap();
+    let s2 = serde_json::to_string(&row2).unwrap();
+    assert_eq!(s1, s2);
+
+    let new_row2: TheirsRow = serde_json::from_str(&s2).unwrap();
+    assert_eq!(new_row2, row2);
+
+    let new_row: OursRow = serde_json::from_str(&s2).unwrap();
+    assert_eq!(new_row, row);
+}
