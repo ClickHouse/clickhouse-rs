62b64130cb072105891bf395438229c315e4bc8e
Austin Bonander <austin.bonander@gmail.com>
2026-08-26 14:12:10 -0700

    refactor: delete useless `serde::uuid_vec::option` module
    As it turns out, ClickHouse doesn't even allow `Nullable(Array(...))`: https://clickhouse.com/docs/reference/data-types/nullable

--- src/serde.rs
+++ src/serde.rs
@@ -114,60 +114,6 @@ pub mod uuid_vec {
     use serde::ser::SerializeSeq;
     use std::fmt;
 
-    #[deprecated = "This module is useless: `Nullable(Array(...))` is not allowed by ClickHouse"]
-    pub mod option {
-        use super::*;
-        use ::uuid::Uuid;
-
-        pub fn serialize<S>(uuids: &Option<Vec<Uuid>>, serializer: S) -> Result<S::Ok, S::Error>
-        where
-            S: Serializer,
-        {
-            match uuids {
-                Some(uuids) => super::serialize(uuids, serializer),
-                None => serializer.serialize_none(),
-            }
-        }
-
-        struct OptionUuidVecVisitor {
-            human_readable: bool,
-        }
-
-        impl<'de> Visitor<'de> for OptionUuidVecVisitor {
-            type Value = Option<Vec<Uuid>>;
-
-            fn expecting(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-                write!(f, "a nullable sequence of UUID values")
-            }
-
-            fn visit_none<E>(self) -> Result<Self::Value, E>
-            where
-                E: Error,
-            {
-                Ok(None)
-            }
-
-            fn visit_some<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
-            where
-                D: Deserializer<'de>,
-            {
-                deserializer
-                    .deserialize_seq(UuidVecVisitor {
-                        human_readable: self.human_readable,
-                    })
-                    .map(Some)
-            }
-        }
-
-        pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<Vec<Uuid>>, D::Error>
-        where
-            D: Deserializer<'de>,
-        {
-            let human_readable = deserializer.is_human_readable();
-            deserializer.deserialize_seq(OptionUuidVecVisitor { human_readable })
-        }
-    }
-
     pub fn serialize<S>(uuids: &[Uuid], serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
